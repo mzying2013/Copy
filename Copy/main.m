2c28f1e74213bf742ac16219c5e70e018c6bef63
Liu <liumin2011@gmail.com>
2018-09-03 14:16:08 +0800

    Initial Commit

---
+++ Copy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Copy
+//
+//  Created by Liu on 2018/9/3.
+//  Copyright © 2018年 mzying.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
